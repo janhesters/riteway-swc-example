87c41692538c6529b52757dea1b81bbc16efea71
Jan Hesters <jan.hesters@rwth-aachen.de>
2022-02-18 00:42:34 +0100

    add things like exclude and include

---
+++ src/home/with-normal-css-component.js
@@ -0,0 +1,18 @@
+import './normal.css';
+
+import { Fragment } from 'react';
+
+import NoCssRelativeImportsComponent from './no-css-relative-imports-component';
+
+export default function WithNormalCSS() {
+  return (
+    <Fragment>
+      <div className="container">
+        <main className="main">
+          <h1>Normal CSS</h1>
+          <NoCssRelativeImportsComponent message="Thank you for the help!" />
+        </main>
+      </div>
+    </Fragment>
+  );
+}
---
+++ src/home/with-normal-css-component.test.js
@@ -0,0 +1,15 @@
+import { describe } from 'riteway';
+import render from 'riteway/render-component';
+
+import WithNormalCSS from './with-normal-css-component';
+
+describe('with normal css component', async assert => {
+  const $ = render(<WithNormalCSS />);
+
+  assert({
+    given: 'just rendering',
+    should: 'render a message where the user is',
+    actual: $('h1').text(),
+    expected: 'Landing Page',
+  });
+});
